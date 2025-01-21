dc9252d9198678deb610d7e26ab301e1b2115c08
Jonathas Ribeiro <jonathas@starti.com.br>
2025-01-20 23:26:40 -0300

    fix ready

--- ready.go
+++ ready.go
@@ -1,1 +1,5 @@
 package duckdb
+
+// Ready implements the ready.Readiness interface, once this flips to true CoreDNS
+// assumes this plugin is ready for queries; it is not checked again.
+func (e DuckDBLogger) Ready() bool { return true }
